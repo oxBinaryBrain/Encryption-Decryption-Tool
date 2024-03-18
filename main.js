fd856aa925a472550c08b788290b7db56d60ee0c
oxBinaryBrain <udayg6269@gmail.com>
2024-03-18 12:31:45 +0530

    encrypt message

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ const rl = readline.createInterface({
 function generateKey() {
     return crypto.randomBytes(32).toString('hex');
 }
+
+function encryptMessage(message, key) {
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), crypto.randomBytes(16));
+    let encryptedMessage = cipher.update(message, 'utf-8', 'hex');
+    encryptedMessage += cipher.final('hex');
+    return encryptedMessage;
+}
+
